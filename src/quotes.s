b8a85483c09c94c4fe974da35228a7cdcf741bb8
agsb <barcellos.alvaro@gmail.com>
2023-10-02 09:30:13 -0300

    added quotes

---
+++ src/quotes.s
@@ -0,0 +1,77 @@
+/*
+ *  DISCLAIMER"
+ *
+ *  Copyright © 2020, Alvaro Gomes Sobral Barcellos,
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions"
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *
+ */
+
+
+messages:    
+;---------------------------------------------------------------------
+;
+;
+    .asciiz "Greetings professor Falken. Shall we play a game ?"
+
+    .asciiz "Embrace the entire problem, Keep it simple, Do not speculate !"
+
+    .asciiz "No keyboard found. Press any key to continue."
+
+    .asciiz "Error 404. Page not found. Release."
+
+    .asciiz "It’s not a bug; it’s an undocumented feature."
+
+    .asciiz "Programming is learned by writing programs."
+
+    .asciiz "1N73LL163NC3 15 7H3 4B1L17Y 70 4D4P7 70 CH4NG35."
+
+    .asciiz "Git commit; Git push; Exit!"
+
+    .asciiz "Don’t comment bad code, just rewrite it." 
+
+    .asciiz "Crash often, but crash early!"
+
+    .asciiz "If you want to go somewhere, a goto is the best way to get there."
+
+    .asciiz "Intelligence is the ability to avoid doing work, yet getting the work done."
+
+    .asciiz "Talk is cheap. Show me the code."
+
+    .asciiz "I really worry about everything going to the cloud."
+
+    .asciiz "Everthing about if, is, not, and, goto, label."
+
+    .asciiz "We've got a problem, HAL."
+
+    .asciiz "The Tao that is seen is not the true Tao."
+    
+    .asciiz "Focus is a matter of deciding what things you are not going to do."
+
+    .asciiz "The program is provided as is, without warranty of any kind."
+
+    .asciiz " I'm afraid I can't do that."
+    
+    .asciiz " My God, it's full of stars."
+    
+    .asciiz "It can only be attributable to human error."
+
+    ; rollete as Pascal made, 32 is red, 15 is black, and so on
+    ; 1/37 is 0,027027027
+    .asciiz "00,32,15,19,04,21,02,25,17,34,06,27,13,36,11,30,08,23,10,05,24,16,33,01,20,14,31,09,22,18,29,07,28,12,35,03,26,"
