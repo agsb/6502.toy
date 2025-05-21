981d77690a3f6ab71b2ae1a51c438d427a844515
agsb <barcellos.alvaro@gmail.com>
2025-05-21 15:01:58 -0300

    clean parameters prepare for sim65 tests

--- src/try/pagging.s
+++ src/try/pagging.s
@@ -1,13 +1,26 @@
 
+.segment "CODE"
+
+;----------------------------------------------------------------------
+;
+;       how much for a context swap ?
+;
+;       moving pages zero and one into/from somewhere
+;       costs about 2060 instructions and 7716 cycles
+;
+;
+;
+;
 
 ;----------------------------------------------------------------------
 ; copy page zero, page one, registers to a swap area
 pagging:
-; address 02 is the destin, must be a page at $XX00
+; address in page is the destin, must be a page at $XX00
 ;   sta (page),y 
 ;   lda (page),y
 ;----------------------------------------------------------------------
 swapinto:
+@reduce:
         php
         pha
         tya
@@ -22,29 +35,30 @@ swapinto:
         lda $0100, y
         sta (PAGE), y
         iny
-        bne loop_one
+        bne @loop_one
         inc PAGE+1
 @loop_zero:
         lda $0000, y
         sta (PAGE), y
         iny
-        bne loop_zero
+        bne @loop_zero
         rts
 
 ;----------------------------------------------------------------------
 swapfrom:
         ; inc PAGE+1
+        ldy #0
 @loop_zero:
         lda (PAGE), y
         sta $0000, y
         iny
-        bne loop_zero
+        bne @loop_zero
         dec PAGE+1
-@loop_zero:
+@loop_one:
         lda (PAGE), y
         sta $0100, y
         iny
-        bne loop_one
+        bne @loop_one
 @restore:        
         pla
         tax
@@ -56,4 +70,26 @@ swapfrom:
         plp
         rts
         
+;----------------------------------------------------------------------
+
+PAGE = $04
+
+;----------------------------------------------------------------------
+
+.import exit
+
+.export _main
+
+_main:
         
+        lda #$00
+        sta PAGE + 0
+        lda #$02
+        sta PAGE + 1
+
+        jsr swapinto
+
+        jsr swapfrom
+        
+        rts
+
